004e57bb2de43db052f08a6fa7c3990e708f328c
Dayo Farayola <dayojnr.3@gmail.com>
2023-10-13 18:27:27 +0100

    add about and work section

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,36 @@
+let currentIndex = 1;
+    
+//Show current image
+showSlides(currentIndex);
+      
+//Function to move Next
+function plusSlides(n) {
+    showSlides(currentIndex += n);
+}
+
+//Function to move back
+function currentSlide(n) {
+    showSlides(currentIndex = n);
+}
+      
+//Initiate moving of slides
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("images");
+    let dots = document.getElementsByClassName("navigation-dot");
+
+    if (n > slides.length) {currentIndex = 1}
+    if (n < 1) {currentIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[currentIndex-1].style.display = "block";
+    dots[currentIndex-1].className += " active";
+}
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav_menu");
 
